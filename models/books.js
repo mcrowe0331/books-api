7b443801aef119ec917f61928074c29b460b93ef
Mary <mcrowe0331@gmail.com>
2022-10-20 13:27:32 -0500

    added schema

---
+++ models/books.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose')
+// const {Schema} = mongoose
+
+const bookSchema = new mongoose.Schema({
+    id: [1],
+    title: { type: String, default: 'The Shinobi Initiative'},
+    description: {type: String, default: 'The reality-bending adventures of a clandestine service agency in the year 2166'},
+    year: { type: Number, default: 2014 },
+    quantity: { type: Number, default: 10 },
+    imageURL: { type: String, default: '/assets/shinobi-initiative.jpeg'}
+  })
+
+  module.exports = mongoose.model('Book', bookSchema)
+
+  //question on id: since it doesn't need a type does it need []
+  
